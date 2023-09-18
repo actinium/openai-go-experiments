ccfea5d12c506fcdc0ccd3a0d727bce530401386
Andreas Cederholm <andreas.cederholm@gmail.com>
2023-09-18 12:57:17 +0200

    Add embeddings client

---
+++ openai/embeddings.go
@@ -0,0 +1,80 @@
+package openai
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+)
+
+const (
+	embeddingsModel      = "text-embedding-ada-002"
+	embeddingsDimensions = 1536
+)
+
+type EmbeddingsClient struct {
+	openaiClient *OpenAIClient
+}
+
+func NewEmbeddingsClient(client *OpenAIClient) *EmbeddingsClient {
+	return &EmbeddingsClient{
+		openaiClient: client,
+	}
+}
+
+type Embedding struct {
+	Text      string
+	Embedding [embeddingsDimensions]float32
+}
+
+type embeddingsRequestPayload struct {
+	Model string `json:"model"`
+	Input string `json:"input"`
+}
+
+type embeddingsResponsePayload struct {
+	Object string `json:"object"`
+	Data   []struct {
+		Object    string                        `json:"object"`
+		Embedding [embeddingsDimensions]float32 `json:"embedding"`
+	} `json:"data"`
+	Model string `json:"model"`
+	Usage struct {
+		PromptTokens uint `json:"prompt_tokens"`
+		TotalTokens  uint `json:"total_tokens"`
+	} `json:"usage"`
+}
+
+func (client *EmbeddingsClient) Create(input string) (Embedding, error) {
+	requestPayload, err := json.Marshal(embeddingsRequestPayload{
+		Model: embeddingsModel,
+		Input: input,
+	})
+	if err != nil {
+		return Embedding{}, err
+	}
+
+	ctx := context.Background()
+	body, err := client.openaiClient.post(ctx, "/embeddings", bytes.NewReader(requestPayload))
+	if err != nil {
+		return Embedding{}, err
+	}
+
+	defer body.Close()
+	content, err := io.ReadAll(body)
+	if err != nil {
+		return Embedding{}, err
+	}
+
+	var responsePayload embeddingsResponsePayload
+	json.Unmarshal(content, &responsePayload)
+	if err != nil {
+		return Embedding{}, err
+	}
+
+	return Embedding{
+		Text:      input,
+		Embedding: responsePayload.Data[0].Embedding,
+	}, nil
+
+}
